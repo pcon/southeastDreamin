87aedf4e6762b55cc6b22d9fe8c57b0721c4d2e3
Patrick Connelly <patrick@deadlypenguin.com>
2020-01-27 17:30:17 -0500

    Adding stuff for 2020 and refactoring code

--- email_acceptedSessions.js
+++ email_acceptedSessions.js
@@ -1,187 +1,87 @@
-/*jslint browser: true, regexp: true */
-/*global require, console */
+const lo = require('lodash');
+const Q = require('q');
+const bunyan = require('bunyan');
 
-var fs = require('fs');
-var lo = require('lodash');
-var handlebars = require('handlebars');
-var Q = require('q');
-var bunyan = require('bunyan');
-
-var logger = bunyan.createLogger({
-    name: "email_acceptedSessions",
+global.logger = bunyan.createLogger({
+    name: 'email_acceptedSessions',
     stream: process.stdout
 });
 
-var sfdc = require('./lib/sfdc.js');
-var email = require('./lib/email.js');
+const sfdc = require('./lib/sfdc.js');
+const utils = require('./lib/utils');
 
-var template = {
+global.template = {
     subject: undefined,
     html: undefined,
     text: undefined
 };
 
-var notified_sessions = [];
-var failed_sessions = [];
+global.notified_sessions = [];
+global.failed_sessions = [];
 
-var readFile = function (fname) {
-    'use strict';
+const EVENT_ID = 'a031K00002u3wYEQAY';
+const EVENT_YEAR = '2020';
 
-    var deferred = Q.defer();
+const FIELDS = [
+    'Id',
+    'Name',
+    'Session_Manager__r.Email',
+    'Main_Presenter__r.FirstName',
+    'Main_Presenter__r.Email'
+];
 
-    fs.readFile(fname, 'utf8', function (err, data) {
-        if (err) {
-            deferred.reject(err);
-        } else {
-            deferred.resolve(data);
-        }
-    });
+const CONDITIONS = [
+    `Event__c = '${EVENT_ID}'`,
+    'RecordType.Name = \'Chosen/Picked\'',
+    'Main_Presenter__r.Email != null',
+    'Session_Status__c = \'Accepted\''
+];
 
-    return deferred.promise;
-};
+const QUERY_PARTS = [
+    'select',
+    lo.join(FIELDS, ','),
+    'from Session__c',
+    'where',
+    lo.join(CONDITIONS, ' and ')
+];
+const QUERY = lo.join(QUERY_PARTS, ' ');
 
-var readTemplates = function () {
-    'use strict';
+const getAcceptedSessions = sfdc.query.bind(null, QUERY);
 
-    var deferred = Q.defer();
-
-    readFile('tmpl/2019/sessionAcceptance.sub')
-        .then(function (subject) {
-            template.subject = handlebars.compile(subject);
-
-            readFile('tmpl/2019/sessionAcceptance.html')
-                .then(function (html) {
-                    template.html = handlebars.compile(html);
-
-                    readFile('tmpl/2019/sessionAcceptance.txt')
-                        .then(function (text) {
-                            template.text = handlebars.compile(text);
-
-                            deferred.resolve();
-                        }).catch(function (err) {
-                            deferred.reject(err);
-                        });
-                }).catch(function (err) {
-                    deferred.reject(err);
-                });
-        }).catch(function (err) {
-            deferred.reject(err);
-        });
-
-
-    return deferred.promise;
-};
-
-var getAcceptedSessions = function () {
-    'use strict';
-
-    var deferred = Q.defer();
-
-    sfdc.query('select Id, Name, Session_Manager__r.Email, Main_Presenter__r.FirstName, Main_Presenter__r.Email from Session__c where Event__c = \'a034100002l1htyAAA\' and RecordType.Name = \'Chosen/Picked\' and Main_Presenter__r.Email != null and Session_Status__c = \'Accepted\'')
-        .then(function (data) {
-            deferred.resolve(data);
-        }).catch(function (err) {
-            deferred.reject(err);
-        });
-
-    return deferred.promise;
-};
-
-var mogrifySessionData = function (sessions) {
-    'use strict';
-
-    var sessionMap = {},
-        deferred = Q.defer();
-
-    lo.forEach(sessions, function (session) {
-        if (!sessionMap.hasOwnProperty(session.Main_Presenter__r.Email)) {
-            sessionMap[session.Main_Presenter__r.Email] = {
-                Ids: [],
-                Email: session.Main_Presenter__r.Email,
-                Name: session.Main_Presenter__r.FirstName,
-                ManagerEmail: session.Session_Manager__r.Email,
-                Sessions: []
-            };
-        }
-
-        sessionMap[session.Main_Presenter__r.Email].Sessions.push({Title: session.Name});
-        sessionMap[session.Main_Presenter__r.Email].Ids.push(session.Id);
-    });
-
-    deferred.resolve(lo.values(sessionMap));
-
-    return deferred.promise;
-};
-
-var sendEmail = function (session) {
-    'use strict';
-
-    var deferred = Q.defer();
-
-    email.send(template, session)
-        .then(function () {
-            logger.info('Email sent for ' + session.Ids);
-            notified_sessions = lo.union(notified_sessions, session.Ids);
-            deferred.resolve();
-        }).catch(function (err) {
-			logger.error('Unable to send email to ' + session.Email + ' - ' + lo.join(session.Ids, ','));
-            failed_sessions = lo.union(failed_sessions, session.Ids);
-            deferred.reject(err);
-        });
-
-    return deferred.promise;
-};
-
-var sendEmails = function (sessions) {
-    'use strict';
-
-    var success = true,
-        promises = [],
-        deferred = Q.defer();
-
-    lo.forEach(sessions, function (session) {
-        promises.push(sendEmail(session));
-    });
-
-    Q.allSettled(promises)
-        .then(function (results) {
-            deferred.resolve();
-        });
-
-    return deferred.promise;
-};
-
+/**
+ * Updates the status of the sessions to notified
+ * @returns {Promise} A promise for when the sessions have been updated
+ */
 var updateStatus = function () {
-    'use strict';
+    var successful_records = [];
+    var deferred = Q.defer();
 
-    var successful_records = [],
-        deferred = Q.defer();
+    lo.forEach(global.notified_sessions, function (id) {
+        successful_records.push({
+            Id: id,
+            Session_Status__c: 'Notified'
+        });
+    });
 
-	lo.forEach(notified_sessions, function (id) {
-		successful_records.push({
-			Id: id,
-			Session_Status__c: 'Notified'
-		});
-	});
-
-	logger.info('Updating ' + lo.size(successful_records) + '\n' + lo.join(notified_sessions, '\n'));
-	sfdc.update('Session__c', successful_records)
-		.then(function () {
-			deferred.resolve();
-		}).catch(function (err) {
-			deferred.reject(err);
-		});
+    global.logger.info('Updating ' + lo.size(successful_records) + '\n' + lo.join(global.notified_sessions, '\n'));
+    sfdc.update('Session__c', successful_records)
+        .then(function () {
+            deferred.resolve();
+        }).catch(function (err) {
+            deferred.reject(err);
+        });
 
     return deferred.promise;
 };
 
-readTemplates()
+utils.readTemplates(EVENT_YEAR, 'sessionAcceptance')
     .then(getAcceptedSessions)
-    .then(mogrifySessionData)
-    .then(sendEmails)
-	.then(updateStatus)
+    .then(utils.mogrifySessionData)
+    .then(function (data) {
+        global.logger.info(data);
+    })
+    .then(utils.sendEmails)
+    .then(updateStatus)
     .catch(function (err) {
-        'use strict';
-
-        logger.error(err);
+        global.logger.error(err);
     });
--- email_rejectedSessions.js
+++ email_rejectedSessions.js
@@ -1,163 +1,64 @@
-/*jslint browser: true, regexp: true */
-/*global require, console */
+const lo = require('lodash');
+const Q = require('q');
+const bunyan = require('bunyan');
 
-var fs = require('fs');
-var lo = require('lodash');
-var handlebars = require('handlebars');
-var Q = require('q');
-var bunyan = require('bunyan');
-
-var logger = bunyan.createLogger({
-    name: "email_declinedSessions",
+global.logger = bunyan.createLogger({
+    name: 'email_declinedSessions',
     stream: process.stdout
 });
 
-var sfdc = require('./lib/sfdc.js');
-var email = require('./lib/email.js');
+const sfdc = require('./lib/sfdc');
+const utils = require('./lib/utils');
 
-var template = {
+global.template = {
     subject: undefined,
     html: undefined,
     text: undefined
 };
 
-var notified_sessions = [];
-var failed_sessions = [];
+global.notified_sessions = [];
+global.failed_sessions = [];
 
-var readFile = function (fname) {
-    'use strict';
+const EVENT_ID = 'a031K00002u3wYEQAY';
+const EVENT_YEAR = '2020';
 
-    var deferred = Q.defer();
+const FIELDS = [
+    'Id',
+    'Name',
+    'Session_Manager__r.Email',
+    'Main_Presenter__r.FirstName',
+    'Main_Presenter__r.Email'
+];
 
-    fs.readFile(fname, 'utf8', function (err, data) {
-        if (err) {
-            deferred.reject(err);
-        } else {
-            deferred.resolve(data);
-        }
-    });
+const CONDITIONS = [
+    `Event__c = '${EVENT_ID}'`,
+    'RecordType.Name = \'Rejected\'',
+    'Main_Presenter__r.Email != null'
+];
 
-    return deferred.promise;
-};
+const QUERY_PARTS = [
+    'select',
+    lo.join(FIELDS, ','),
+    'from Session__c',
+    'where',
+    lo.join(CONDITIONS, ' and '),
+    'order by Main_Presenter__r.Email'
+];
+const QUERY = lo.join(QUERY_PARTS, ' ');
 
-var readTemplates = function () {
-    'use strict';
+const getDeclinedSessions = sfdc.query.bind(null, QUERY);
 
-    var deferred = Q.defer();
-
-    readFile('tmpl/2019/sessionRejection.sub')
-        .then(function (subject) {
-            template.subject = handlebars.compile(subject);
-
-            readFile('tmpl/2019/sessionRejection.html')
-                .then(function (html) {
-                    template.html = handlebars.compile(html);
-
-                    readFile('tmpl/2019/sessionRejection.txt')
-                        .then(function (text) {
-                            template.text = handlebars.compile(text);
-
-                            deferred.resolve();
-                        }).catch(function (err) {
-                            deferred.reject(err);
-                        });
-                }).catch(function (err) {
-                    deferred.reject(err);
-                });
-        }).catch(function (err) {
-            deferred.reject(err);
-        });
-
-
-    return deferred.promise;
-};
-
-var getDeclinedSessions = function () {
-    'use strict';
-
-    var deferred = Q.defer();
-
-    sfdc.query('select Id, Name, Main_Presenter__r.FirstName, Main_Presenter__r.Email from Session__c where Event__c = \'a034100002l1htyAAA\' and RecordType.Name = \'Rejected\' and Main_Presenter__r.Email != null order by Main_Presenter__r.Email')
-        .then(function (data) {
-            deferred.resolve(data);
-        }).catch(function (err) {
-            deferred.reject(err);
-        });
-
-    return deferred.promise;
-};
-
-var mogrifySessionData = function (sessions) {
-    'use strict';
-
-    var sessionMap = {},
-        deferred = Q.defer();
-
-    lo.forEach(sessions, function (session) {
-        if (!sessionMap.hasOwnProperty(session.Main_Presenter__r.Email)) {
-            sessionMap[session.Main_Presenter__r.Email] = {
-                Ids: [],
-                Email: session.Main_Presenter__r.Email,
-                Name: session.Main_Presenter__r.FirstName,
-                Sessions: []
-            };
-        }
-
-        sessionMap[session.Main_Presenter__r.Email].Sessions.push({Title: session.Name});
-        sessionMap[session.Main_Presenter__r.Email].Ids.push(session.Id);
-    });
-
-    deferred.resolve(lo.values(sessionMap));
-
-    return deferred.promise;
-};
-
-var sendEmail = function (session) {
-    'use strict';
-
-    var deferred = Q.defer();
-
-    email.send(template, session)
-        .then(function () {
-            logger.info('Email sent for ' + session.Ids);
-            notified_sessions = lo.union(notified_sessions, session.Ids);
-            deferred.resolve();
-        }).catch(function (err) {
-            logger.error('Unable to send email to ' + session.Email + ' - ' + session.Id);
-            failed_sessions = lo.union(failed_sessions, session.Ids);
-            deferred.reject(err);
-        });
-
-    return deferred.promise;
-};
-
-var sendEmails = function (sessions) {
-    'use strict';
-
-    var promises = [],
-        deferred = Q.defer();
-
-    lo.forEach(sessions, function (session) {
-        promises.push(sendEmail(session));
-    });
-
-    Q.allSettled(promises)
-        .then(function () {
-            deferred.resolve();
-        });
-
-    return deferred.promise;
-};
-
+/**
+ * Set the record type to Rejected
+ * @returns {Promise} A promise for when the sessions have been updated
+ */
 var updateRecordTypes = function () {
-    'use strict';
-
-    var successful_records = [],
-        deferred = Q.defer();
+    var successful_records = [];
+    const deferred = Q.defer();
 
     sfdc.query('select Id from RecordType where Name = \'Rejected\'')
         .then(function (data) {
-            lo.forEach(notified_sessions, function (id) {
+            lo.forEach(global.notified_sessions, function (id) {
                 successful_records.push({
                     Id: id,
                     RecordTypeId: data[0].Id
@@ -177,21 +78,17 @@ var updateRecordTypes = function () {
     return deferred.promise;
 };
 
-readTemplates()
+utils.readTemplates(EVENT_YEAR, 'sessionRejection')
     .then(getDeclinedSessions)
-    .then(mogrifySessionData)
-    .then(sendEmails)
-//    .then(updateRecordTypes)
+    .then(utils.mogrifySessionData)
+    .then(utils.sendEmails)
+    .then(updateRecordTypes)
     .then(function () {
-        'use strict';
+        global.logger.info('Notified');
+        global.logger.info(global.notified_sessions);
 
-        logger.info('Notified');
-        logger.info(notified_sessions);
-
-        logger.info('Failed');
-        logger.info(failed_sessions);
+        global.logger.info('Failed');
+        global.logger.info(global.failed_sessions);
     }).catch(function (err) {
-        'use strict';
-
-        logger.error(err);
+        global.logger.error(err);
     });
